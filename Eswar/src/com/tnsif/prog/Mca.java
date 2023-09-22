afaa78f3cc141122a599a50702aeb8f583b83d98
madir <madir@10.0.0.133>
2023-09-22 14:28:05 +0530

    firstcommit

---
+++ Eswar/src/com/tnsif/prog/Mca.java
@@ -0,0 +1,10 @@
+package com.tnsif.prog;
+
+public class Mca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("good morning to all");
+	}
+
+}

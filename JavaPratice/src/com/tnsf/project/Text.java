8c4eecf1d123b7dc8de1fbfd9fb8171edbd9e3f6
madir <madir@Kirans__PC>
2023-10-09 13:55:11 +0530

    first commit

---
+++ JavaPratice/src/com/tnsf/project/Text.java
@@ -0,0 +1,11 @@
+package com.tnsf.project;
+
+public class Text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Susmitha");
+
+	}
+
+}

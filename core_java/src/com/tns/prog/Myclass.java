62d205f8fe07061a1fde1f48b54161fe60c4eff0
SvkpSkill2 <SvkpSkill2@10.0.0.123>
2023-10-12 14:35:13 +0530

    third commit

---
+++ core_java/src/com/tns/prog/Myclass.java
@@ -0,0 +1,18 @@
+package com.tns.prog;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value=(int)(9/2.0d);//explicit
+		double value2=101d/6d;
+		double value3=101/61;
+		System.out.println("value="+ value);
+		System.out.println("value2="+ value2);
+		System.out.println("value3="+ value3);
+
+	}
+
+}
+
+	

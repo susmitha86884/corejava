d91d971fe6be86fda23c2cacc98e9473465bc1cb
SvkpSkill2 <SvkpSkill2@10.0.0.120>
2023-10-13 09:59:18 +0530

    Fourth commit

---
+++ core_java/src/com/tnsif/prog2/Fiftthmultiple.java
@@ -0,0 +1,14 @@
+package com.tnsif.prog2;
+
+public class Fiftthmultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,five_m;//1
+		a=22;//2 static initialization
+		five_m=5*a;//dynamic
+		System.out.println("The fifth multiple of 22 is" + five_m);		
+
+	}
+
+}
